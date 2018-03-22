6cf6ca2a332600a7f71832e16345a6c4f539d9e7
singlee <mdd6691@gmail.com>
2018-03-22 16:46:00 +0800

    更新接口

--- src/tools/servers.js
+++ src/tools/servers.js
@@ -1,40 +1,39 @@
 import axios from 'axios'
 import qs from 'qs'
 import debug from '../../config/debug.json'
-import { Msg } from './message'
-
-const isLogin = window.sessionStorage.getItem('user.isLogin')
-const token = window.sessionStorage.getItem('user.token')
+import {Msg} from './message'
 
 /**
  * 请求类
  */
 export class BaseApi {
-  servers
+  servers;
 
   /**
    * 初始化servers
    * @param hot
    */
-  constructor (hot) {
-    const headers = new Headers()
-    headers.set('App-Version', '0.1.0')
-    if (isLogin) headers.set('Authorization', token)
+  constructor(hot) {
+    const headers = new Headers();
+    headers.set('App-Version', '0.1.0');
     this.servers = axios.create({
-      baseURL: `${hot}/api`, // http://moe.cn-su.net/api 测试用
+      baseURL: `${hot}/`,
       headers,
       timeout: 1,
-    })
-    this.servers.defaults.timeout = 5000
+    });
+    this.getStatusToken();
+    this.servers.defaults.timeout = 5000;
     this.servers.interceptors.request.use(function (config) {
       return config
     }, function (error) {
-      Msg.error(error.message)
-    })
+      Msg.error(error);
+      return Promise.reject(error);
+    });
     this.servers.interceptors.response.use(function (response) {
       return response
     }, function (error) {
-      Msg.error(error.message)
+      Msg.error(error.response.data.message);
+      return Promise.reject(error.response.data.message);
     })
   }
 
@@ -45,15 +44,37 @@ export class BaseApi {
    * @param body
    * @returns {Promise<any>}
    */
-  connection (method = 'GET', url, body) {
-    if (typeof body !== 'object') body = {}
-    method = method.toLocaleLowerCase()
-    if (method === 'get' || method === 'head' || method === 'delete') {
-      url = `${url}?${qs.stringify(body)}`
+  connection(method = 'GET', url, body) {
+    if (typeof body !== 'object') body = {};
+    method = method.toLocaleLowerCase();
+    if (method === 'get') {
+      url = `${url}?${qs.stringify(body)}`;
       body = {}
     }
+    this.getStatusToken();
     return Promise.resolve(this.servers[method](url, body))
+  }
+
+  /**
+   * 设置token
+   * @param isLogin
+   * @param token
+   */
+  setToken({isLogin, token}) {
+    if (isLogin) {
+      this.servers.defaults.headers.common['Authorization'] = token;
+    }
+  }
+
+  /**
+   * 获取登录状态，token值
+   * @returns {{isLogin: string, token: string}}
+   */
+  getStatusToken() {
+    const isLogin = window.sessionStorage.getItem('user.isLogin');
+    const token = window.sessionStorage.getItem('user.token');
+    this.setToken({isLogin, token})
   }
 }
 
-export const server = new BaseApi(debug.servers)
+export const server = new BaseApi(debug.servers);
